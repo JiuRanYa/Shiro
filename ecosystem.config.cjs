a3fdc5930eeec655c8ea429e8b9b80ae3d7ecad2
JiuRanYa <1084350607@qq.com>
2025-03-24 15:05:19 +0800

    chore(ecosystem): update deployment path in configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -21,7 +21,7 @@ module.exports = {
       host : '60.205.234.149', // 远程服务器地址，需要配ssh登录
       ref  : 'origin/master', // 需要部署的分支
       repo : 'git@github.com:JiuRanYa/Shiro.git', // 代码git地址，需要配ssh登录
-      path : '/var/www/production', // 文件存放的地址
+      path : '/site/shiro', // 文件存放的地址
       'post-deploy' : 'npm install && npm run build' // 拉完代码需要执行的构建命令
     }
   }
