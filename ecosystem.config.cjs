b8906aab36664e04d13c79e5354c81e33b280b14
JiuRanYa <1084350607@qq.com>
2025-03-24 17:00:11 +0800

    chore(ecosystem): refine deployment path and clear pre-setup command in configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -21,8 +21,8 @@ module.exports = {
       host: '60.205.234.149',
       ref: 'origin/master',
       repo: 'git@github.com:JiuRanYa/Shiro.git',
-      path: '/site/shiro',
-      'pre-setup': 'rm -rf /site/shiro/source/shiro',
+      path: '/site/Shiro',
+      'pre-setup': '',
       'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.cjs',
       'pre-deploy-local': '',
       'ssh_options': 'StrictHostKeyChecking=no'
