2bbd43a68526015fc8c6416bd31faaec23dc14fc
JiuRanYa <1084350607@qq.com>
2025-03-27 10:12:14 +0800

    chore(ecosystem): refactor ecosystem configuration to define application settings for Shiro

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,17 @@
 module.exports = {
-  deploy: {
-    production: {
-      user: 'root',
-      host: '60.205.234.149',
-      ref: 'origin/main',
-      repo: 'git@github.com:JiuRanYa/Shiro.git',
-      path: '/site/Shiro',
-      'pre-setup': '',
-      'post-deploy': 'pnpm install && pnpm run build',
-      'pre-deploy-local': '',
-      'ssh_options': 'StrictHostKeyChecking=no'
-    }
-  }
+  apps: [
+    {
+      name: 'Shiro',
+      script: 'npx next start -p 2323',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        NEXT_SHARP_PATH: process.env.NEXT_SHARP_PATH,
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+  ],
 }
